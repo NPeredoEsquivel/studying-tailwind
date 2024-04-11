fd871fbfc535e22c1f70d815fe99a1dcfddfa704
Nicolas Peredo <nicolas.peredo@globant.com>
2024-04-11 16:54:16 -0400

    Adding styles to the new component NoProjectSelected

--- src/components/NoProjectSelected/NoProjectSelected.tsx
+++ src/components/NoProjectSelected/NoProjectSelected.tsx
@@ -6,10 +6,10 @@ type Props = {}
 const NoProjectSelected = (props: Props) => {
   return (
     <div className="mt-24 text-center w-2/3">
-      <img src={noProject}/>
-      <h2>No Project Selected</h2>
-      <p>Select a projet or get started with a new one</p>
-      <p>
+      <img src={noProject} alt="An empty task list" className="w-16 h-16 object-contain mx-auto"/>
+      <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
+      <p className="text-stone-400 mb-4">Select a projet or get started with a new one</p>
+      <p className="mt-8">
         <button>
           Create New Project
         </button>
