0a9b781047b850d961596ad1cf911df793d58795
Nicolas Peredo <nicolas.peredo@globant.com>
2024-04-11 16:41:45 -0400

    Adding styles to input and textarea

--- src/UI/Input.tsx
+++ src/UI/Input.tsx
@@ -10,9 +10,9 @@ type Props = {
 const Input: React.FC<Props> = ({ label, props }) => {
 
   return (
-    <p>
-      <label htmlFor={label.toLowerCase()}>{label}</label>
-      <input {...props} />
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500" htmlFor={label.toLowerCase()}>{label}</label>
+      <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} />
     </p>
   )
 }
--- src/UI/Textarea.tsx
+++ src/UI/Textarea.tsx
@@ -8,8 +8,8 @@ type Props = {
 const Textarea: React.FC<Props> = ({ label, props }) => {
   return (
     <p>
-      <label htmlFor={label.toLowerCase()}>{label}</label>
-      <textarea {...props}></textarea>
+      <label className="text-sm font-bold uppercase text-stone-500" htmlFor={label.toLowerCase()}>{label}</label>
+      <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props}></textarea>
     </p>
   )
 }
