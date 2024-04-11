4dca43b1d75f73e1799cb254110e78cbca7cf899
Nicolas Peredo <nicolas.peredo@globant.com>
2024-04-11 17:05:24 -0400

    Creating a new Button component

--- src/components/NewProject/NewProject.tsx
+++ src/components/NewProject/NewProject.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Input from '../../UI/Input';
 import Textarea from '../../UI/Textarea';
+import Button from '../../UI/Button';
 
 type Props = {}
 
@@ -8,8 +9,8 @@ const NewProject: React.FC <Props> = ({}: Props) =>  {
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
-        <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-        <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+        <li><Button className="text-stone-800 hover:text-stone-950">Cancel</Button></li>
+        <li><Button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</Button></li>
       </menu>
       <div>
           <Input label={"Title"} />
