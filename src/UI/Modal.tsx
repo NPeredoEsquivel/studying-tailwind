c2c63c0d228e76dfaf5e01dc94b4fe66365ed449
Nicolas Peredo <nicolas.peredo@globant.com>
2024-04-17 08:05:13 -0400

    Adding reusable Modal

---
+++ src/UI/Modal.tsx
@@ -0,0 +1,36 @@
+import React, { useImperativeHandle } from 'react'
+import { createPortal } from 'react-dom';
+
+
+type Props = {
+  children: React.ReactNode
+}
+
+type ModalRef = {
+  open: () => void;
+  close: () => void;
+
+}
+
+const Modal = React.forwardRef<ModalRef, Props>(({ children }, ref) => {
+  const dialogRef = React.useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => ({
+      open: () => {
+        const dialog = dialogRef.current as HTMLDialogElement;
+        dialog.showModal();
+      },
+      close: () => {
+        const dialog = dialogRef.current as HTMLDialogElement;
+        dialog.close();
+      }
+    }));
+
+  return createPortal(
+    <dialog ref={dialogRef}>
+      {children}
+    </dialog>,
+    document.getElementById('modal-root') as HTMLElement
+  )
+})
+export default Modal;
